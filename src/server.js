5059387c09967a9bbb148020b907d7f2f45ce12c
AlexReid95 <alexreid95@gmail.com>
2020-05-16 12:56:41 +0100

    Add some comments

--- src/server.js
+++ src/server.js
@@ -13,14 +13,13 @@ server.use(express.urlencoded());
 server.use(cookieParser());
 server.use(express.static("./public"));
 
-
-
 // Routes
 // ------- Generic handlers -------
+// Renders template html
 server.get("/", genericHandlers.homeHandler);
 
 // ------- User handlers -------
-// Renders templates html which contains a form
+// Renders template html
 server.get("/log-in", userHandlers.getLoginHandler);
 server.get("/sign-up", userHandlers.getSignUpHandler);
 server.get("/log-out", userHandlers.getLogoutHandler);
@@ -29,16 +28,12 @@ server.get("/log-out", userHandlers.getLogoutHandler);
 server.post("/log-in", userHandlers.postLoginHandler);
 server.post("/sign-up", userHandlers.postSignUpHandler);
 
-
-
-
-
 // npm test => When PGDATABASE=localtest, we are testing, server does not need to be running
 if (process.env.PGDATABASE !== "localtest") {
-    // We start the server listening on a specific port
-    server.listen(PORT, () =>
-        console.log(`Server listening on http:localhost:${PORT}`)
-    );
+	// We start the server listening on a specific port
+	server.listen(PORT, () =>
+		console.log(`Server listening on http:localhost:${PORT}`)
+	);
 }
 
 module.exports = server;
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -4,57 +4,74 @@ const build = require("../src/db/build");
 const supertest = require("supertest");
 const test = require("tape");
 
-
-test("Route tests are running!", t => {
-    const x = 2;
-    t.equal(x, 2, "Routes initial test is working");
-    t.end();
+test("Route tests are running!", (t) => {
+	const x = 2;
+	t.equal(x, 2, "Routes initial test is working");
+	t.end();
 });
 
-test("Test / get route", t => {
-    build()
-        .then(() => {
-            supertest(server)
-                .get("/")
-                .expect(200)
-                .expect("content-type", "text/html; charset=utf-8")
-                .end((err, res) => {
-                    t.error(err, "Status is 200 and content-type is text/html; charset=utf-8");
-                    t.equals(res.text.includes("<h1>Link Hoarder</h1>"), true, "<h1>HOME</h1> should be present");
-                    t.end();
-                });
-        })
-})
+test("Test / get route", (t) => {
+	build().then(() => {
+		supertest(server)
+			.get("/")
+			.expect(200)
+			.expect("content-type", "text/html; charset=utf-8")
+			.end((err, res) => {
+				t.error(
+					err,
+					"Status is 200 and content-type is text/html; charset=utf-8"
+				);
+				t.equals(
+					res.text.includes("<h1>Link Hoarder</h1>"),
+					true,
+					"<h1>HOME</h1> should be present"
+				);
+				t.end();
+			});
+	});
+});
 
-test("Test /sign-up get route", t => {
-    build()
-        .then(() => {
-            supertest(server)
-                .get("/sign-up")
-                .expect(200)
-                .expect("content-type", "text/html; charset=utf-8")
-                .end((err, res) => {
-                    t.error(err, "Status is 200 and content-type is text/html; charset=utf-8");
-                    t.equals(res.text.includes('class="form signup-form"'), true, "Signup form should be present");
-                    t.end();
-                });
-        })
-})
+test("Test /sign-up get route", (t) => {
+	build().then(() => {
+		supertest(server)
+			.get("/sign-up")
+			.expect(200)
+			.expect("content-type", "text/html; charset=utf-8")
+			.end((err, res) => {
+				t.error(
+					err,
+					"Status is 200 and content-type is text/html; charset=utf-8"
+				);
+				t.equals(
+					res.text.includes('class="form signup-form"'),
+					true,
+					"Signup form should be present"
+				);
+				t.end();
+			});
+	});
+});
 
-test("Test /log-in get route", t => {
-    build()
-        .then(() => {
-            supertest(server)
-                .get("/log-in")
-                .expect(200)
-                .expect("content-type", "text/html; charset=utf-8")
-                .end((err, res) => {
-                    t.error(err, "Status is 200 and content-type is text/html; charset=utf-8");
-                    t.equals(res.text.includes('class="form login-form"'), true, "Login form should be present");
-                    t.end();
-                });
-        })
-})
+test("Test /log-in get route", (t) => {
+	build().then(() => {
+		supertest(server)
+			.get("/log-in")
+			.expect(200)
+			.expect("content-type", "text/html; charset=utf-8")
+			.end((err, res) => {
+				t.error(
+					err,
+					"Status is 200 and content-type is text/html; charset=utf-8"
+				);
+				t.equals(
+					res.text.includes('class="form login-form"'),
+					true,
+					"Login form should be present"
+				);
+				t.end();
+			});
+	});
+});
 
 // test("Test /sign-up post route", t => {
 //     build()
@@ -66,8 +83,6 @@ test("Test /log-in get route", t => {
 //                 .then((req, res) => {
 //                     assert(res.body.username, "she-ra")
 //                 })
-
-
 
 //                 .end((err, res) => {
 //                     t.error(err);
